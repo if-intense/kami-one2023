2467033ce29e897c7d4ccfc6a4aa26a27065cb8e
インテンス <intense@inifinv.local>
2023-01-24 16:34:07 +0900

    yes

---
+++ assets/js/list.js
@@ -0,0 +1,25 @@
+var vm = new Vue ({
+    el: '#list',
+    data: {
+        stylists: [
+            {id: 1, name: "阿部 吉晃（ALLY・東京都）", team: false},
+            {id: 2, name: "大田JAPAN（OCEAN TOKYO OVER・東京都）", team: true},
+            {id: 3, name: "TAKERU（JOMELL.・東京都）", team: true},
+            {id: 4, name: "坂本 たかまさ（ACA・北海道）", team: false},
+            {id: 5, name: "島崎 力斗（OCEAN TOKYO・東京都）", team: false},
+            {id: 6, name: "末石 稔（PEEK-A-BOO 原宿・東京都）", team: true},
+            {id: 7, name: "田代 時男（SHIMA・東京都）", team: true},
+            {id: 8, name: "CHO （ANTI・東京都）", team: true},
+            {id: 9, name: "堤 大輔（LiLoinveve・大阪府）", team: false},
+            {id: 10, name: "箱崎 貴彦（PEEK-A-BOO 原宿・東京都）", team: true},
+            {id: 11, name: "橋ヶ谷 桃子（PEEK-A-BOO銀座並木通り・東京都）", team: true},
+            {id: 12, name: "張替 光輝（ヒロ銀座バーバーショップ ・神奈川県）", team: false},
+            {id: 13, name: "藤原 愛莉 （Belle表参道・東京都）", team: false},
+            {id: 14, name: "藤巻 巡日人（romp・東京都）", team: false},
+            {id: 15, name: "峰田 大暉（OCEAN TOKYO WHITE・東京都）", team: false},
+            {id: 16, name: "山野 俊貴（Lond・東京都）", team: true},
+            {id: 17, name: "横井 澄愛（cachecache 表参道・東京都）", team: true},
+            {id: 18, name: "RYUSEI （PELE・東京都）", team: true},
+        ]
+    }
+});
